320cfa6807c7b3fad507776788a4e1bd1e34b1f9
Melike Vurucu <melikechan@tuta.io>
2024-09-05 22:31:00 +0300

    qiskit: change circuit simulator - As IBM Cloud QASM Simulator is deprecated, utilized fake providers for RNG simulation. (however, input flexiblity is limited)

--- README.md
+++ README.md
@@ -3,11 +3,19 @@
 Generate testcases for your competitive programming problems with the randomness of the qubits!
 
 ## Features
-- Generate random 32-bit numbers using IBM Quantum Experience.
+
+- Generate pseudo-random numbers using IBM fake providers (local simulators) and IBM Quantum Platform (real quantum computers).
+  - It works through mapping the random number to the desired range.
+  - However, the range is works **as expected** if the number is between $-2^(n-1)$ and $2^(n-1) - 1$. (where $n$ is the number of qubits, one bit is used for the sign)
+- Generate random graphs with the number of nodes and edges.
+
+**Important note:** In an 16GB RAM machine, maximum number of qubits with $10^5$ random numbers is 14.
 
 ## Upcoming Features
+
 - Using the random number generator, generate testcases, from arrays to graphs.
 - Easy-to-describe, why would you change the code for every problem?
 
 ## Contributing
+
 Pull requests are very welcome, you can create a pull request from the corresponding GitHub section.
--- src/rng_utils.py
+++ src/rng_utils.py
@@ -1,19 +1,31 @@
 import config
 import qiskit
-from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2
+from qiskit_ibm_runtime.fake_provider import FakeAuckland
 
 
 class RandomNumberGenerator:
-    num_qubits = 32
+    num_qubits = 14  # For the sake of randomness, we can increase the number of qubits
+    cloud_mode = False  # Use it at your own risk, it may cost you money.
+
     def __init__(self):
         self.init_runtime()
         self.init_circuit()
 
     def init_runtime(self):
         # Load the IBM Quantum Runtime service
-        self.runtime = QiskitRuntimeService(
-            channel="ibm_quantum", token=config.quantumPlatformToken)
-        self.backend = self.runtime.backend("ibmq_qasm_simulator")
+        if self.cloud_mode:
+            self.runtime = QiskitRuntimeService(
+                channel="ibm_quantum", token=config.quantumPlatformToken
+            )
+            backends = self.runtime.backends(min_num_qubits=self.num_qubits)
+
+            if len(backends) == 0:
+                raise Exception("No backends available")
+
+            self.backend = backends[0]
+        else:
+            self.backend = FakeAuckland()
 
     # Quantum circuit to generate random numbers, classical register to store the results
     def init_circuit(self):
@@ -24,22 +36,32 @@ class RandomNumberGenerator:
     # Generate multiple random numbers in the range [l, r].
     # The number of random numbers to generate is specified by num_numbers.
     def get_random_number(self, l: int, r: int, num_numbers: int = 1) -> list:
-        # Generate a 32 bit random number
         self.circ.h(self.qreg)
         self.circ.measure(self.qreg, self.creg)
 
-        # Execute the circuit using the IBM Quantum Runtime
-        job = self.backend.run(self.circ, shots=num_numbers)
-        results = [k for k, v in job.result(
-        ).results[0].data.counts.items() if v == 1]
+        transpiled_circ = qiskit.transpile(self.circ, backend=self.backend)
+
+        # Execute the circuit
+        sampler = SamplerV2(self.backend)
+        job = sampler.run([transpiled_circ], shots=num_numbers)
+        if self.cloud_mode:
+            result = job.result(timeout=60)
+        else:
+            result = job.result()[0].data.c0
 
         # Convert the results to integers and scale them to the desired range
-        results = [l + int(result, 16) % (r - l + 1) for result in results]
+        results = [
+            (int(key[1:], 2) * (1 if key[0] == "0" else -1)) % (r - l + 1) + l
+            for key in result.get_counts().keys()
+        ]
 
         return results
 
 
 if __name__ == "__main__":
+    # Sample usage
+    """
     rng = RandomNumberGenerator()
-    print(rng.get_random_number(0, 100, 10))    
-    # print(get_random_number(0, 100, 10))
+    a = rng.get_random_number(-(2**13), 2**13 - 1, 10)
+    print(a)
+    """
