c9a391e3e1581f81429992cbc800dfa24fca75a8
Melike Vurucu <melikechan@tuta.io>
2024-09-14 13:50:08 +0300

    GraphGenerator: major structural changes - Added properties to validate graphs, like if a graph is cyclic, it couldn't be a tree etc. - Extract graph builder function from individual modes. (like matrix etc.) - Add cycle detecting mechanism.
    [Not tested]

--- src/generators.py
+++ src/generators.py
@@ -1,7 +1,8 @@
 import rng_utils
 
-
 # Numeric Values
+
+
 class BaseGenerator:
     def __init__(self):
         self.rng = rng_utils.RandomNumberGenerator()
@@ -58,14 +59,135 @@ class ArrayGenerator(BaseGenerator):
 
 
 class GraphGenerator(BaseGenerator):
-    # TODO - Implement tree-bipartite etc. graph
-    def __init__(self, n: int, m: int, weight: list[int], weight_sum: int, directed: bool):
+    def __init__(self, n: int, m: int, weight: list[int], weight_sum: int = None,
+                 directed: bool = False, cyclic: bool = False,
+                 self_loops: bool = False, multi_edges: bool = False,
+                 tree: bool = False):
         super().__init__()
         self.n = n
         self.m = m
         self.weight = weight  # [l, r]
+
+        self.weighted = weight != None
         self.weight_sum = weight_sum
+
+        self._directed = False
+
+        self._cyclic = False
+        self._self_loops = False
+        self._multi_edges = False
+        self._tree = False
+
         self.directed = directed
+        self.cyclic = cyclic
+        self.self_loops = self_loops
+        self.multi_edges = multi_edges
+        self.tree = tree
+
+        self.graph = {{} for _ in range(n)}
+
+    @property
+    def tree(self) -> bool:
+        return self._tree
+
+    @tree.setter
+    def tree(self, value: bool):
+        self._tree = value
+
+        if value:
+            self._cyclic = False
+            self._multi_edges = False
+
+            self.m = self.n - 1
+
+    @property
+    def cyclic(self) -> bool:
+        return self._cyclic
+
+    @cyclic.setter
+    def cyclic(self, value: bool):
+        self._cyclic = value
+
+        if value:
+            self._tree = False
+        else:
+            self._self_loops = False
+            self._directed = True
+
+    def check_cycle(self) -> bool:
+        visited = [False] * self.n
+        stack = []
+
+        stack.append(0)
+        visited[0] = True
+
+        while stack:
+            u = stack.pop()
+
+            for v in self.graph[u]:
+                if not visited[v]:
+                    stack.append(v)
+                    visited[v] = True
+                else:
+                    return True
+
+    def build_graph(self):
+        current_sum = None if self.weight_sum == None else 0
+
+        for _ in range(self.m):
+            u, v = self.rng.get_random_number(0, self.n - 1, 2)
+            w = (
+                self.rng.get_random_number(self.weight[0], self.weight[1])[0]
+                if self.weight != None and len(self.weight) == 2
+                else 1
+            )
+
+            if not self.multi_edges and v in self.graph[u]:
+                continue
+
+            self.graph[u][v] = w
+
+            if current_sum != None:
+                current_sum += w
+
+            if not self.directed:
+                self.graph[v][u] = w
+
+        # TODO: Check graph in a more optimized way
+        if not self._cyclic:
+            if self.check_cycle():
+                self.build_graph()
+
+        if self.weight_sum != None:
+            while current_sum != self.weight_sum:
+                diff = self.weight_sum - current_sum
+
+                u = self.rng.get_random_number(0, self.n - 1)[0]
+                v = self.rng.get_random_number(0, self.n - 1)[0]
+
+                if not self.multi_edges and v in self.graph[u]:
+                    continue
+
+                if diff > 0:
+                    w = self.rng.get_random_number(
+                        self.weight[0], self.weight[1])[0]
+
+                    current_sum -= self.graph[u][v]
+
+                    self.graph[u][v] = min(
+                        diff, self.weight[1] - self.graph[u][v])
+
+                    current_sum += self.graph[u][v]
+                else:
+                    w = self.rng.get_random_number(
+                        self.weight[0], self.weight[1])[0]
+
+                    current_sum -= self.graph[u][v]
+
+                    self.graph[u][v] = max(
+                        diff, self.weight[0] - self.graph[u][v])
+
+                    current_sum += self.graph[u][v]
 
     def get_graph(self, mode: str) -> list:
         if mode == "adj_matrix":
@@ -78,158 +200,47 @@ class GraphGenerator(BaseGenerator):
             raise Exception("Invalid mode")
 
     def get_matrix(self) -> list:
-        adj_matrix = [[0 for _ in range(self.n)] for _ in range(self.n)]
+        if self.multi_edges:
+            raise Exception("Cannot generate matrix for multi-edges")
 
-        current_sum = 0 if self.weight_sum == None else self.weight_sum
-        for _ in range(self.m):
-            u, v = self.rng.get_random_number(0, self.n - 1, 2)
-            w = (
-                self.rng.get_random_number(self.weight[0], self.weight[1])[0]
-                if self.weight != None and len(self.weight) == 2
-                else 1
-            )
-            adj_matrix[u][v] = w
+        if not self.weighted:
+            adj_matrix = [[0] * self.n for _ in range(self.n)]
 
-            if current_sum != None:
-                current_sum += w
+            for u in range(self.n):
+                for v in self.graph[u]:
+                    adj_matrix[u][v] = 1
+        else:
+            adj_matrix = [[0] * self.n for _ in range(self.n)]
 
-            if not self.directed:
-                adj_matrix[v][u] = w
-
-        # Adjust the weights if needed
-        if self.weight_sum != None:
-            while current_sum != self.weight_sum:
-                diff = self.weight_sum - current_sum
-
-                u = self.rng.get_random_number(0, self.n - 1)[0]
-                v = self.rng.get_random_number(0, self.n - 1)[0]
-
-                if adj_matrix[u][v] == 0:
-                    continue
-
-                if diff > 0:
-                    w = self.rng.get_random_number(
-                        self.weight[0], self.weight[1])[0]
-
-                    current_sum -= adj_matrix[u][v]
-
-                    adj_matrix[u][v] = min(
-                        diff, self.weight[1] - adj_matrix[u][v])
-
-                    current_sum += adj_matrix[u][v]
-                else:
-                    w = self.rng.get_random_number(
-                        self.weight[0], self.weight[1])[0]
-
-                    current_sum -= adj_matrix[u][v]
-
-                    adj_matrix[u][v] = max(
-                        diff, self.weight[0] - adj_matrix[u][v])
-
-                    current_sum += adj_matrix[u][v]
+            for u in range(self.n):
+                for v in self.graph[u]:
+                    adj_matrix[u][v] = self.graph[u][v]
 
         return adj_matrix
 
     def get_adj_list(self) -> list:
         adj_list = [[] for _ in range(self.n)]
 
-        current_sum = 0 if self.weight_sum != None else None
-        for _ in range(self.m):
-            u, v = self.rng.get_random_number(0, self.n - 1, 2)
-            w = (
-                self.rng.get_random_number(self.weight[0], self.weight[1])[0]
-                if self.weight != None and len(self.weight) == 2
-                else 1
-            )
-            adj_list[u].append((v, w))
-
-            if current_sum != None:
-                current_sum += w
-
-            if not self.directed:
-                adj_list[v].append((u, w))
-
-        # Adjust the weights if needed
-        if self.weight_sum != None:
-            while current_sum != self.weight_sum:
-                diff = self.weight_sum - current_sum
-
-                u = self.rng.get_random_number(0, self.n - 1)[0]
-                if adj_list[u] == []:
-                    continue
-
-                v = self.rng.get_random_number(0, len(adj_list[u]) - 1)[0]
-
-                if diff > 0:
-                    w = self.rng.get_random_number(
-                        self.weight[0], self.weight[1])[0]
-
-                    current_sum -= adj_list[u][v][1]
-
-                    adj_list[u][v] = (adj_list[u][v][0], min(
-                        diff, self.weight[1] - adj_list[u][v][1]))
-
-                    current_sum += adj_list[u][v][1]
+        for u in range(self.n):
+            for v in self.graph[u]:
+                if not self.weighted:
+                    adj_list[u].append(v)
                 else:
-                    w = self.rng.get_random_number(
-                        self.weight[0], self.weight[1])[0]
-
-                    current_sum -= adj_list[u][v][1]
-
-                    adj_list[u][v] = (adj_list[u][v][0], max(
-                        diff, self.weight[0] - adj_list[u][v][1]))
-
-                    current_sum += adj_list[u][v][1]
+                    adj_list[u].append((v, self.graph[u][v]))
 
         return adj_list
 
     def get_edge_list(self) -> list:
         edge_list = []
 
-        current_sum = 0 if self.weight_sum != None else None
-        for _ in range(self.m):
-            u, v = self.rng.get_random_number(0, self.n - 1, 2)
-            w = (
-                self.rng.get_random_number(self.weight[0], self.weight[1])[0]
-                if self.weight != None and len(self.weight) == 2
-                else 1
-            )
-            edge_list.append((u, v, w))
-
-            if current_sum != None:
-                current_sum += w
-
-            if not self.directed:
-                edge_list.append((v, u, w))
-
-        # Adjust the weights if needed
-        if self.weight_sum != None:
-            while current_sum != self.weight_sum:
-                diff = self.weight_sum - current_sum
-                edge_number = self.rng.get_random_number(0, self.m - 1)[0]
-                if diff > 0:
-                    w = self.rng.get_random_number(
-                        self.weight[0], self.weight[1])[0]
-
-                    current_sum -= edge_list[edge_number][2]
-
-                    edge_list[edge_number][2] = min(
-                        diff, self.weight[1] - edge_list[edge_number][2])
-
-                    current_sum += edge_list[edge_number][2]
+        for u in range(self.n):
+            for v in self.graph[u]:
+                if not self.weighted:
+                    edge_list.append((u, v))
                 else:
-                    w = self.rng.get_random_number(
-                        self.weight[0], self.weight[1])[0]
-
-                    current_sum -= edge_list[edge_number][2]
-
-                    edge_list[edge_number][2] = max(
-                        diff, self.weight[0] - edge_list[edge_number][2])
-
-                    current_sum += edge_list[edge_number][2]
+                    edge_list.append((u, v, self.graph[u][v]))
 
         return edge_list
-
 
 class StringGenerator:
     # TODO
