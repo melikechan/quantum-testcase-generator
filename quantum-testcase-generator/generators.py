fc6363197cf51dc9336dd05978ff8d08a2706997
Melike Vurucu <melikechan@tuta.io>
2024-09-27 16:37:52 +0300

    generator: add docstrings and improve graph generator - Improved variable namings for graph generator. - Added setter for node and edge counts. - Added docstring to generators except graph generator.

--- quantum-testcase-generator/generators.py
+++ quantum-testcase-generator/generators.py
@@ -3,21 +3,48 @@ import random
 
 
 class BaseGenerator:
+    """
+    Base class for all numeric generators, including a random number generator.
+    """
+
     def __init__(self):
         self.rng = rng_utils.RandomNumberGenerator()
 
 
 class SingleNumberGenerator(BaseGenerator):
+    """
+    Class for generating a single number within a given range.
+
+    Args:
+        l (int): Lower bound of the range.
+        r (int): Upper bound of the range.
+    """
+
     def __init__(self, l: int, r: int):
         super().__init__()
         self.l = l
         self.r = r
+
+    """
+    Get a single random number within the given range [l, r].
+
+    Returns:
+        int: Random number within the given range [l, r].
+    """
 
     def get_number(self) -> int:
         return self.rng.get_random_number(self.l, self.r)[0]
 
 
 class ArrayGenerator(BaseGenerator):
+    """
+    Class for generating an array of random numbers within a given range.
+
+    Args:
+        l (int): Lower bound of the range.
+        r (int): Upper bound of the range.
+    """
+
     def __init__(self, l: int, r: int):
         super().__init__()
         self.l = l
@@ -45,6 +72,13 @@ class ArrayGenerator(BaseGenerator):
 
         if value is not None and (value < self.l * self.num_numbers or value > self.r * self.num_numbers):
             raise Exception("Invalid sum")
+
+    """
+    Get an array of random numbers within the given range [l, r].
+
+    Returns:
+        list: Array of random numbers within the given range [l, r].
+    """
 
     def get_array(self) -> list:
         result = []
@@ -80,13 +114,12 @@ class ArrayGenerator(BaseGenerator):
 
 
 class GraphGenerator(BaseGenerator):
-    def __init__(self, n: int, m: int, weight: list[int] = None, weight_sum: int = None,
-                 directed: bool = False, cyclic: bool = False,
-                 self_loops: bool = False, multi_edges: bool = False,
-                 tree: bool = False):
+    def __init__(self, node_count: int, edge_count: int, weight: list[int] = None, weight_sum: int = None):
         super().__init__()
-        self.n = n
-        self.m = m
+
+        self._node_count = node_count
+        self._edge_count = edge_count
+
         self.weight = weight  # [l, r]
 
         self.weighted = weight != None
@@ -99,13 +132,7 @@ class GraphGenerator(BaseGenerator):
         self._multi_edges = False
         self._tree = False
 
-        self.directed = directed
-        self.cyclic = cyclic
-        self.self_loops = self_loops
-        self.multi_edges = multi_edges
-        self.tree = tree
-
-        self.graph = {{} for _ in range(n)}
+        self.graph = [{} for _ in range(node_count)]
 
     @property
     def tree(self) -> bool:
@@ -119,7 +146,7 @@ class GraphGenerator(BaseGenerator):
             self._cyclic = False
             self._multi_edges = False
 
-            self.m = self.n - 1
+            self.edge_count = self.node_count - 1
 
     @property
     def cyclic(self) -> bool:
@@ -135,8 +162,71 @@ class GraphGenerator(BaseGenerator):
             self._self_loops = False
             self._directed = True
 
+    @property
+    def self_loops(self) -> bool:
+        return self._self_loops
+
+    @self_loops.setter
+    def self_loops(self, value: bool):
+        self._self_loops = value
+
+        if value:
+            self._cyclic = True
+
+    @property
+    def multi_edges(self) -> bool:
+        return self._multi_edges
+
+    @multi_edges.setter
+    def multi_edges(self, value: bool):
+        self._multi_edges = value
+
+        if value:
+            self._cyclic = True
+
+    @property
+    def directed(self) -> bool:
+        return self._directed
+
+    @directed.setter
+    def directed(self, value: bool):
+        self._directed = value
+
+    @property
+    def weight_sum(self) -> int:
+        return self._weight_sum
+
+    @property
+    def node_count(self) -> int:
+        return self._node_count
+
+    @node_count.setter
+    def node_count(self, value: int):
+        self._node_count = value
+
+        # Arrange the graph
+        if value < len(self.graph):
+            self.graph = self.graph[:value]
+        elif value > len(self.graph):
+            self.graph += [{} for _ in range(value - len(self.graph))]
+
+    @property
+    def edge_count(self) -> int:
+        return self._edge_count
+
+    @edge_count.setter
+    def edge_count(self, value: int):
+        self._edge_count = value
+
+    @weight_sum.setter
+    def weight_sum(self, value: int):
+        self._weight_sum = value
+
+        if value is not None and (value < self.l * self.edge_count or value > self.r * self.edge_count):
+            raise Exception("Invalid weight sum")
+
     def check_cycle(self) -> bool:
-        visited = [False] * self.n
+        visited = [False] * self.node_count
         stack = []
 
         stack.append(0)
@@ -155,8 +245,8 @@ class GraphGenerator(BaseGenerator):
     def build_graph(self):
         current_sum = None if self.weight_sum == None else 0
 
-        for _ in range(self.m):
-            u, v = self.rng.get_random_number(0, self.n - 1, 2)
+        for _ in range(self.edge_count):
+            u, v = self.rng.get_random_number(0, self.node_count - 1, 2)
             w = (
                 self.rng.get_random_number(self.weight[0], self.weight[1])[0]
                 if self.weight != None and len(self.weight) == 2
@@ -183,8 +273,8 @@ class GraphGenerator(BaseGenerator):
             while current_sum != self.weight_sum:
                 diff = self.weight_sum - current_sum
 
-                u = self.rng.get_random_number(0, self.n - 1)[0]
-                v = self.rng.get_random_number(0, self.n - 1)[0]
+                u = self.rng.get_random_number(0, self.node_count - 1)[0]
+                v = self.rng.get_random_number(0, self.node_count - 1)[0]
 
                 if not self.multi_edges and v in self.graph[u]:
                     continue
@@ -213,8 +303,6 @@ class GraphGenerator(BaseGenerator):
     def get_graph(self, mode: str) -> list:
         if mode == "adj_matrix":
             return self.get_matrix()
-        elif mode == "adj_list":
-            return self.get_adj_list()
         elif mode == "edge_list":
             return self.get_edge_list()
         else:
@@ -225,36 +313,26 @@ class GraphGenerator(BaseGenerator):
             raise Exception("Cannot generate matrix for multi-edges")
 
         if not self.weighted:
-            adj_matrix = [[0] * self.n for _ in range(self.n)]
+            adj_matrix = [
+                [0] * self.node_count for _ in range(self.node_count)]
 
-            for u in range(self.n):
+            for u in range(self.node_count):
                 for v in self.graph[u]:
                     adj_matrix[u][v] = 1
         else:
-            adj_matrix = [[0] * self.n for _ in range(self.n)]
+            adj_matrix = [
+                [0] * self.node_count for _ in range(self.node_count)]
 
-            for u in range(self.n):
+            for u in range(self.node_count):
                 for v in self.graph[u]:
                     adj_matrix[u][v] = self.graph[u][v]
 
         return adj_matrix
 
-    def get_adj_list(self) -> list:
-        adj_list = [[] for _ in range(self.n)]
-
-        for u in range(self.n):
-            for v in self.graph[u]:
-                if not self.weighted:
-                    adj_list[u].append(v)
-                else:
-                    adj_list[u].append((v, self.graph[u][v]))
-
-        return adj_list
-
     def get_edge_list(self) -> list:
         edge_list = []
 
-        for u in range(self.n):
+        for u in range(self.node_count):
             for v in self.graph[u]:
                 if not self.weighted:
                     edge_list.append((u, v))
